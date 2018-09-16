f06869cc7cd01c35758bcb7347b3f7a768788309
imYashGupta <37147192+imYashGupta@users.noreply.github.com>
2018-09-17 03:22:08 +0530

    documentation Added for some functions

--- function.php
+++ function.php
@@ -3,116 +3,96 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 date_default_timezone_set('Asia/Calcutta');
 define('host', 'localhost');
-define('dbuser', 'makerite_ecom');
-define('dbpass', 'unr#A1RnG8yB');
-define('dbname', 'makerite_ecom');
+define('dbuser', 'root');
+define('dbpass', '');
+define('dbname', 'test');
 define('site_url', $_SERVER['SERVER_NAME']);
-define('site_name', 'Ecom' );
-define('site_email','Help@ecom.com');
-/*
-required php 7.0+
+define('site_name', 'website.co m' );
+define('site_email','example@website.com');
 
-Example to use
-
-SaveData("tblname",["name"=> "Yash","last_name" => "Gupta","full_name" => "Yash Gupta" ,"email" => "email@mail.com","password" => '5646'],'created_at');
-
-*/
+$GLOBALS["conn"]=mysqli_connect(host,dbuser,dbpass,dbname);
 ob_start();
-session_start();
-if(isset($_SESSION['user']))
-{
-	$fullname=$_SESSION['user']["fullname"];
-	$email=$_SESSION['user']["email"];
-	$lastname=$_SESSION['user']["lastname"];
-	$firstname=$_SESSION['user']["firstname"];
-	$address=$_SESSION['user']["address"];
-	$postcode=$_SESSION['user']["postcode"];
-	$phone=$_SESSION['user']["phone"];
-	$image=$_SESSION['user']['image'];
-}
 
-if(isset($_SESSION["admin"])){
-	$firstname=$_SESSION["admin"]["firstname"];
-	$lastname=$_SESSION["admin"]["lastname"];
-	$fullname=$firstname.' '.$lastname;
-	$email=$_SESSION["admin"]["email"];
-	$token=$_SESSION["admin"]["token"];
-}
-
-$conn=mysqli_connect(host,dbuser,dbpass,dbname);
-
-
-function saveData(string $tbl_name,array $tbl_value,string $datetime='')
+ 
+/**
+ * You don,t need to write sqls to inserting a new row into database ,Just use this function for easier syntax
+ * 
+ * Example to Use:
+ * $dataToInsert=["columnName" => "value" , "columnName2" => "value"];
+ * insertInto("tblname",$dataToInsert);
+ * 
+ *
+ * @param      string  $tbl_name   Name of Your Table
+ * @param      array   $tbl_value  keys and values 
+ * @param      string  $datetime   a optional param if have any createdAt column in your table, this will insert datetime!
+ *
+ * @return     <type>  ( query return true or false )
+ */
+function insertInto(string $tbl_name,array $tbl_value,string $datetime='')
 {   
-	$conn=mysqli_connect(host,dbuser,dbpass,dbname);
+	 
  	$string='';
 	$stringValue='';
 	foreach ($tbl_value as $key => $value) {
 	 	 $string .=$key.",";
 	 } 
 	foreach ($tbl_value as $key => $value) {
-		 $stringValue .="'".mysqli_real_escape_string($conn,$value)."',";
+		 $stringValue .="'".mysqli_real_escape_string($GLOBALS["conn"],$value)."',";
 	}
 	   $tbl_key= substr($string,0,-1);
 	   $tbl_value=substr($stringValue,0,-1);
 	   if (!empty($datetime)) {
-	     $mysqliQuery=mysqli_query($conn,"INSERT INTO $tbl_name($tbl_key,$datetime)VALUES($tbl_value,NOW())");
+	     $mysqliQuery=mysqli_query($GLOBALS["conn"],"INSERT INTO $tbl_name($tbl_key,$datetime)VALUES($tbl_value,NOW())");
 	   }
 	   else{
-	   $mysqliQuery=mysqli_query($conn,"INSERT INTO $tbl_name($tbl_key)VALUES($tbl_value)");
+	   $mysqliQuery=mysqli_query($GLOBALS["conn"],"INSERT INTO $tbl_name($tbl_key)VALUES($tbl_value)");
 		}
 	   return $mysqliQuery;
 }
 
-/*
-	Example to use
-	select("tblname",condition);	
 
-
-
-*/
-
-function select(string $tableName,string $condition=''){
-	$conn=mysqli_connect(host,dbuser,dbpass,dbname);
-
+/**
+ * For Fetching single or multiple rows from database
+ * 
+ * Example to Use:
+ * selectFrom("tblname","id='1' AND status='active'");
+ *
+ * @param      string  $tableName  Name of Your Table
+ * @param      string  $condition  conditions
+ *
+ * @return     <type>  ( query return arrays)
+ */
+function selectFrom(string $tableName,string $condition=''){
 	if (empty($condition)) {
-		$fetchQuery=mysqli_query($conn,"SELECT * FROM $tableName");
+		$fetchQuery=mysqli_query($GLOBALS["conn"],"SELECT * FROM $tableName");
 	}
 	else{
-		$fetchQuery=mysqli_query($conn,"SELECT * FROM $tableName WHERE $condition");
+		$fetchQuery=mysqli_query($GLOBALS["conn"],"SELECT * FROM $tableName WHERE $condition");
 	}
 	
 	return $fetchQuery;
 }
-
 /*
 	Example to use
 	howMany(query);	
-
 */
-
 function howMany($query){
 	$mysqli_num_rows=mysqli_num_rows($query);
 	return $mysqli_num_rows;
 }
-
 /*
 	Example to use
 	fetch(query);	
-
 */
-
 function fetch($query){
 	$mysqli_fetch_array=mysqli_fetch_array($query);
 	return $mysqli_fetch_array;
 }
-
 /* Random string
 	Example to use
 	$a = token(32);
 	$b = token(8, 'abcdefghijklmnopqrstuvwxyz');
 */
-
 function token($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 {
     $pieces = [];
@@ -123,14 +103,21 @@ function token($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFG
     return implode('', $pieces);
 }
 
-/* Random int
-	Example to use
-	$a = tokenInt(32);
-	$b = tokenInt(8, 'abcdefghijklmnopqrstuvwxyz');
-*/
 
 
 
+/**
+ * For Genrating Random string of int or numbers 
+ * 
+ * Example to Use:
+ * tokenInt(8);
+ * this will return 8 char long stings i.e 56215465
+ *
+ * @param      integer  $length    specify the length of strings
+ * @param      string   $keyspace  optional param for selected int
+ *
+ * @return     <type>   ( returns a random numbers strings )
+ */
 function tokenInt($length, $keyspace = '0123456789')
 {
     $pieces = [];
@@ -140,21 +127,16 @@ function tokenInt($length, $keyspace = '0123456789')
     }
     return implode('', $pieces);
 }
-
 /*
 	delete
 	Example to use
 	deleteRow("tblname","condition");
-
-
 */
-
 function deleteRow(string $tblname,string $condition)
-{	$conn=mysqli_connect(host,dbuser,dbpass,dbname);
-	$deleteQuery=mysqli_query($conn,"DELETE FROM $tblname WHERE $condition");
+{	 
+	$deleteQuery=mysqli_query($GLOBALS["conn"],"DELETE FROM $tblname WHERE $condition");
 	return $deleteQuery;
 }
-
 /**
 	 * @param  string $tblname [name of your table]
 	 * @param  array  $var_value [name and value for column in table to be update]
@@ -166,18 +148,17 @@ function deleteRow(string $tblname,string $condition)
 	 */
 	function update(string $tblname,array $var_value,string $condition,string $updated_at='')
 	{
-		$conn=mysqli_connect(host,dbuser,dbpass,dbname);
-		$query=mysqli_query($conn,"SHOW columns FROM $tblname");
+		$query=mysqli_query($GLOBALS["conn"],"SHOW columns FROM $tblname");
 		$strings='';
 		$keys='';
 		$error='';
 		foreach ($var_value as $key => $value) {
-			$strings .= $key."='".mysqli_real_escape_string($conn,$value)."',";
+			$strings .= $key."='".mysqli_real_escape_string($GLOBALS["conn"],$value)."',";
 			$keys .=$key.",";
 		}
 		$queryBuild=substr($strings,0,-1);
 		if (empty($updated_at)) {
-			$updateQuery=mysqli_query($conn,"UPDATE $tblname SET $queryBuild WHERE $condition");
+			$updateQuery=mysqli_query($GLOBALS["conn"],"UPDATE $tblname SET $queryBuild WHERE $condition");
 		}
 		else{
 			$arr='';
@@ -199,10 +180,9 @@ function deleteRow(string $tblname,string $condition)
  			$errorArray=explode(',',$error);
  			$errorCount=count($errorArray);
  			$totalError=$errorCount-1;
-
  			if ($totalError <= 0) {
  				if (array_search($updated_at,$array)) {
-					$updateQuery=mysqli_query($conn,"UPDATE $tblname SET $queryBuild,$updated_at=NOW() WHERE $condition");
+					$updateQuery=mysqli_query($GLOBALS["conn"],"UPDATE $tblname SET $queryBuild,$updated_at=NOW() WHERE $condition");
 				}
 				else{
 					echo $updateQuery="<b>Error!</b> There is no Field in Table <b>".$tblname."</b> with <b>".$updated_at."</b> column.";
@@ -219,14 +199,19 @@ function deleteRow(string $tblname,string $condition)
 		return $updateQuery;
 	}
 
-
+	/**
+	 * { For Redirection }
+	 *
+	 * @param      string   $path  link Or Path of the file where you want redirection
+	 *
+	 * @return     boolean  ( this will return js function window.location to redirect )
+	 */
 	function move(string $path)
 	{
 		//header("Location:$path");
 		echo "<script>window.location.href='$path';</script>";
 		return true;
 	}
-
 	function secureAdmin()
 	{
 		if (!isset($_SESSION["admin"]) AND empty($_SESSION["admin"])) {
@@ -234,16 +219,12 @@ function deleteRow(string $tblname,string $condition)
 		}
 		return true;
 	}
-
-
 /*
 	if You call this method this will check about `user` Session And redirection to Index.php if no pere is give 
 	or if you want custom redirection give one single perameter as page url!
-
 */
 		function secureUser($link='')
 	{
-
 		if (!isset($_SESSION["user"]) AND empty($_SESSION["user"])) {
 			if ($link!="") {
 				move("https://".site_url."/".$link);
@@ -256,11 +237,17 @@ function deleteRow(string $tblname,string $condition)
 		return true;
 	}
 
+
+
+	/**
+	 * Gets the user ip.
+	 *
+	 * @return     <type>  The user ip.
+	 */
 	function getUserIP(){
 	    $client  = @$_SERVER['HTTP_CLIENT_IP'];
 	    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
 	    $remote  = $_SERVER['REMOTE_ADDR'];
-
 	    if(filter_var($client, FILTER_VALIDATE_IP))
 	    {
 	        $ip = $client;
@@ -273,10 +260,17 @@ function deleteRow(string $tblname,string $condition)
 	    {
 	        $ip = $remote;
 	    }
-
 	    return $ip;
 		}
 
+
+		/**
+		 * { this will convert datetime into more readable format i.e 5 minutes ago , a week ago etc. }
+		 *
+		 * @param      <type>  $timestamp  2018-09-15 22:54:48
+		 *
+		 * @return     string  
+		 */
 		function timeAgo($timestamp){  
 	      $time_ago = strtotime($timestamp);  
 	      $current_time = time();  
@@ -360,52 +354,58 @@ function deleteRow(string $tblname,string $condition)
 	   }  
 	 }
 
+
+	/**
+	 * { to format datetime or timestamp, this will format timestamp like 2018-09-15 22:54:48 into this Sep 15,2018 }
+	 *
+	 * @param      string  $datetime  timestamp or datetime i.e 2018-09-15 22:54:48
+	 * @param      string  $format    datetime format i.e y,m,d,h,s etc
+	 *
+	 * @return     <type>  ( returns a read able format of timestamps )
+	 */
 	function dateFormat(string $datetime,string $format)
 	 {
 	 	$date=date_create($datetime);
 		return date_format($date,$format);
 	 }
 
-
+	/**
+	 * { This function is work exact like mysqli_real_escape_string() instead of Writing long strings simply use realEscape() }
+	 *
+	 * @param      string  $str  The string to be escaped. 
+	 *
+	 * @return     <type>  Returns an escaped string.
+	 */
 	function realEscape(string $str){
-		$conn=mysqli_connect(host,dbuser,dbpass,dbname);
-		return mysqli_real_escape_string($conn,$str);
+		 
+		return mysqli_real_escape_string($GLOBALS["conn"],$str);
 	} 
+
 
 	function post(string $str)
 	{
-		$conn=mysqli_connect(host,dbuser,dbpass,dbname);
-		$index=mysqli_real_escape_string($conn,$str);
+		 
+		$index=mysqli_real_escape_string($GLOBALS["conn"],$str);
 		return $_POST[$index];
 	}
-
 	function lastID(string $tblName)
 	{
-		$conn=mysqli_connect(host,dbuser,dbpass,dbname);
-		$select=mysqli_query($conn," SELECT * FROM $tblName ORDER BY id DESC LIMIT 1 ");
+		 
+		$select=mysqli_query($GLOBALS["conn"]," SELECT * FROM $tblName ORDER BY id DESC LIMIT 1 ");
 		$row=mysqli_fetch_array($select);
 		return $row["id"];
 	}
-
 	function query(string $str)
 	{
-		$conn=mysqli_connect(host,dbuser,dbpass,dbname);
-		return mysqli_query($conn,$str);
+		return mysqli_query($GLOBALS["conn"],$str);
 	}
-
 	function response(array $array)
 	{
 		return exit(json_encode(["response" => $array]));
 	}
-
 	function returnJson(int $int,string $str)
 	{
 		return exit(json_encode(["response" => ["code" => $int ,"msg" => $str]]));
 	}
-
  
-
-
-
-
 ?>
